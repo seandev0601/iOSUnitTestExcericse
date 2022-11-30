d775a4b2c1b51fa31a6a1380eb95e1de274fc311
Sean <seanbopo@gmail.com>
2022-11-30 14:29:25 +0800

    Initial Commit

---
+++ PhotoApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PhotoApp
+//
+//  Created by ChaoBo-Shang on 2022/11/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
