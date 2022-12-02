1ed364a67c02b33b53481d05d8160824950c77fb
Sean <seanbopo@gmail.com>
2022-12-02 15:58:45 +0800

    Implement SingupWebServiceTests with real http request

--- PhotoAppTests/Signup/Network/SingupWebServiceTests.swift
+++ PhotoAppTests/Signup/Network/SingupWebServiceTests.swift
@@ -6,6 +6,7 @@
 //
 
 import XCTest
+@testable import PhotoApp
 
 class SingupWebServiceTests: XCTestCase {
 
@@ -17,4 +18,22 @@ class SingupWebServiceTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
     
+    func testSingupWebService_WhenGivenSuccessfullResponse_ReturnsSuccess() {
+        // Arrange
+        let sut = SignupWebService(urlString: "https://tlyqhtlbn8.execute-api.us-east-1.amazonaws.com/prod/signup-mock-service/users")
+        
+        let signFormRequestModel = SignupFormRequestModel(firstName: "Sergy", lastName: "Kargopolov", email: "test@test.com", password: "12345678")
+        
+        let expectation = self.expectation(description: "Signup Web Service Response Expectation")
+        
+        // Act
+        sut.singup(withForm: signFormRequestModel) { (signupResponseModel, error) in
+            // Assert
+            XCTAssertEqual(signupResponseModel?.status, "ok")
+            expectation.fulfill()
+        }
+        
+        self.wait(for: [expectation], timeout: 5)
+    }
+
 }
