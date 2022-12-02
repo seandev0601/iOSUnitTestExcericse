d478ddaaf761d915258352ada10baf216ae88750
Sean <seanbopo@gmail.com>
2022-12-02 14:15:13 +0800

    Implement SignupFormModelValidator

---
+++ PhotoApp/SignupConstants.swift
@@ -0,0 +1,17 @@
+//
+//  SignupConstants.swift
+//  PhotoApp
+//
+//  Created by ChaoBo-Shang on 2022/12/1.
+//
+
+import Foundation
+
+struct SignupConstants {
+    static let firstNameMinLength = 2
+    static let firstNameMaxLength = 10
+    static let lastNameMinLength = 2
+    static let lastNameMaxLength = 10
+    static let passwordMinLength = 8
+    static let passwordMaxLength = 16
+}
---
+++ PhotoApp/SignupFormModelValidator.swift
@@ -0,0 +1,55 @@
+//
+//  SignupFormModelValidator.swift
+//  PhotoApp
+//
+//  Created by ChaoBo-Shang on 2022/12/1.
+//
+
+import Foundation
+
+class SignupFormModelValidator {
+    
+    func isFirstNameValid(firstName: String) -> Bool {
+        var returnValue = true
+        
+        if firstName.isEmpty || firstName.count < SignupConstants.firstNameMinLength || firstName.count > SignupConstants.firstNameMaxLength {
+            returnValue = false
+        }
+        
+        return returnValue
+    }
+    
+    func isLastNameValid(lastName: String) -> Bool {
+        var returnValue = true
+        
+        if lastName.isEmpty ||
+            lastName.count < SignupConstants.lastNameMinLength ||
+            lastName.count > SignupConstants.lastNameMaxLength {
+            returnValue = false
+        }
+        
+        return returnValue
+    }
+    
+    func isValidEmailFormat(email: String) -> Bool {
+        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}"
+        let emailTest = NSPredicate(format: "SELF MATCHES %@", emailRegEx)
+        return emailTest.evaluate(with: email)
+    }
+    
+    func isPasswordValid(password: String) -> Bool {
+        var returnValue = true
+        
+        if password.isEmpty ||
+            password.count < SignupConstants.passwordMinLength ||
+            password.count > SignupConstants.passwordMaxLength {
+            returnValue = false
+        }
+        
+        return returnValue
+    }
+    
+    func doPasswordMatch(password: String, repeatPassword: String) -> Bool {
+        return password == repeatPassword
+    }
+}
