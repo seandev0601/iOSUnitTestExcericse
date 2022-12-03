858cfafe6f1d4c8d836c2f8c97ac90634122fae5
Sean <seanbopo@gmail.com>
2022-12-03 11:15:11 +0800

    Implement MockURLProtocol

---
+++ PhotoAppTests/Signup/MockURLProtocol.swift
@@ -0,0 +1,31 @@
+//
+//  MockURLProtocol.swift
+//  PhotoAppTests
+//
+//  Created by ChaoBo-Shang on 2022/12/3.
+//
+
+import Foundation
+
+class MockURLProtocol: URLProtocol {
+    
+    static var stubResponseData: Data?
+    
+    override class func canInit(with request: URLRequest) -> Bool {
+        return true
+    }
+
+    override class func canonicalRequest(for request: URLRequest) -> URLRequest {
+        return request
+    }
+    
+    override func startLoading() {
+        self.client?.urlProtocol(self, didLoad: MockURLProtocol.stubResponseData ?? Data())
+        
+        self.client?.urlProtocolDidFinishLoading(self)
+    }
+    
+    override func stopLoading() {
+        
+    }
+}
